0c11921bfc5eb8c7fa65be5e891fbf8c922a4d4c
emabel <emabel@student.21-school.ru>
2022-03-06 18:51:23 +0300

    инициация

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import './components/Todo'
+import { Todo } from './components/Todo';
+
+function App() {
+
+
+  return (
+    <div className="App">
+		<Todo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Todo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Todo: React.FC = () => {
+
+
+	return (
+		<div>
+			dfdfcdsdsd
+		</div>
+	)
+}
