07f94ac8a44a0938a98e245d0b96859262639124
emabel <emabel@student.21-school.ru>
2022-03-07 10:09:02 +0300

    добавление базовой логики

--- frontend/src/components/Todo.tsx
+++ frontend/src/components/Todo.tsx
@@ -1,21 +1,35 @@
-import React from "react";
-import { useState } from "react";
-import { TodoList }  from './TodoList'
+import React from 'react'
+import { useState } from 'react'
+import { TodoList } from './TodoList'
 
 export const Todo: React.FC = () => {
+  const [todos, setTodos] = useState<string[]>([])
+  const [input, setInput] = useState('')
 
-	const [todos, setTodos ] = useState([])
-	const [input, setInput] = useState('')
+  
+  const handleChange = (e: any) => {
+    setInput(e.target.value)
+  }
 
-	const handleChange = (e: any) => {
-		setInput(e.target.value)
-	}
+  const addTodo = () => {
+    const newArr = [...todos, input]
+    setTodos(newArr)
+	setInput('')
+  }
 
-	return (
-		<div>
-			<input type="text" onChange={handleChange} />
+  return (
+    <div>
+      <input type="text" onChange={handleChange} value={input} />
+      <button onClick={addTodo}>Добавить новую задачу</button>
+      {/* <p>{input}</p> */}
 
-			<p>{input}</p>
-		</div>
-	)
+      <ul>
+		  {todos.map((todo, i) => (
+			  <li key={i}>
+				  {todo}
+			  </li>
+		  ))}
+	  </ul>
+    </div>
+  )
 }
